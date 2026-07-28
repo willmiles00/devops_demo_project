9384725acec28b320ff261b440f206d60e3dc287
Will <113927655+willmiles00@users.noreply.github.com>
2026-07-28 16:25:56 +0000

    added routing

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("hello world")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "  hello   "
+
+@app.route("/new")
+def new():
+    return "new site "
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
